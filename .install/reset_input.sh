33194e64d3a69b39b2cdda00ce4f79c6c7cf86fc
Tubbles <jae91m@gmail.com>
2019-01-19 12:17:11 +0100

    Add touchpad gestures, better settings

---
+++ .install/reset_input.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+setxkbmap -layout us -variant alt-intl-unicode
+xset r rate 200 60
+
+if [[ -n "$(command -v xinput)" ]] ; then
+    # Set up touch pad
+    tpid="$(xinput list | grep TouchPad | sed -E 's/.*id=([0-9]+).*/\1/g')"
+    if [[ -n "${tpid}" ]] ; then
+        function xinp-set {
+            if [[ -n "$(xinput list-props $1 | grep $2)" ]] ; then
+                xinput set-prop $1 $2 $3
+            fi
+        }
+
+        # Enable tap clicking
+        xinp-set ${tpid} 293 1
+        # Enable tap click dragging lock
+        xinp-set ${tpid} 297 1
+        # Enable natural ("mac") scrolling direction
+        xinp-set ${tpid} 287 1
+        # Do not disable while typing
+        xinp-set ${tpid} 301 0
+        # Set the click method
+        xinp-set ${tpid} 307 "{0 1}"
+        # Tapping button mapping
+        #xinp-set ${tpid} 299 {1 1}
+        # Middle mouse button emulation. This disables the middle mouse soft area
+        xinp-set ${tpid} 309 1
+    fi
+fi
