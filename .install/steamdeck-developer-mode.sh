81679ac6dce7a6c726bcce1635104f942ce9ca15
Tubbles <jae91m@gmail.com>
2022-11-27 21:17:53 +0100

    Add developer install script for steamdeck

---
+++ .install/steamdeck-developer-mode.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -x
+
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+
+packages=(
+    "atool"
+    "base-devel"
+    "fontconfig"
+    "freetype2"
+    "fzf"
+    "gcc"
+    "glibc"
+    "highlight"
+    "holo/linux-headers"
+    "holo/linux-lts-headers"
+    "imlib2"
+    "librender"
+    "libx11"
+    "libxft"
+    "libxrender"
+    "linux-api-headers"
+    "linux-neptune-headers"
+    "lld"
+    "lldb"
+    "lynx"
+    "mlocate"
+    "ranger"
+    "rust-analyzer-nightly-bin"
+    "rustup"
+    "tmux"
+    "w3m"
+    "xorgproto"
+)
+
+yay -S ${packages[@]}
+rustup toolchain install nightly
+yay -S helix-git
