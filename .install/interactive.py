d0b1a5a91fae17e2ef70208192ad03257fdf3f45
Tubbles <jae91m@gmail.com>
2023-08-23 07:29:57 +0200

    pyi: Add frac()

--- .install/interactive.py
+++ .install/interactive.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
+# /usr/local/bin/st -g100x30 -e python3 -i /home/tubbles/.install/interactive.py
+# /usr/local/bin/st -g44x30 -e R -q --no-save
+
 from fractions import Fraction  # NOQA
+
+
+def frac(a, b):
+    return Fraction(a, b).as_integer_ratio()
