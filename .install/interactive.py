c846ada7753792e6bf1494b036fad6118b8c8f52
Tubbles <jae91m@gmail.com>
2024-04-02 16:40:21 +0200

    pyi: to*(): Add bin and oct, allow input parameter

--- .install/interactive.py
+++ .install/interactive.py
@@ -10,27 +10,65 @@ def frac(a, b):
     return Fraction(a, b).as_integer_ratio()
 
 
-def todec():
-    try:
-        while True:
-            print("> ", end="")
-            a = input()
-            try:
-                print(int(a, 0))
-            except ValueError as e:
-                print(e)
-    except (KeyboardInterrupt, EOFError):
-        print("")
+def todec(number=None):
+    if not number:
+        try:
+            while True:
+                print("> ", end="")
+                number = input()
+                try:
+                    print(int(str(number), 0))
+                except ValueError as e:
+                    print(e)
+        except (KeyboardInterrupt, EOFError):
+            print("")
+    else:
+        print(int(str(number), 0))
 
 
-def tohex():
-    try:
-        while True:
-            print("> ", end="")
-            a = input()
-            try:
-                print(hex(int(a, 0)))
-            except ValueError as e:
-                print(e)
-    except (KeyboardInterrupt, EOFError):
-        print("")
+def tohex(number=None):
+    if not number:
+        try:
+            while True:
+                print("> ", end="")
+                number = input()
+                try:
+                    print(hex(int(str(number), 0)))
+                except ValueError as e:
+                    print(e)
+        except (KeyboardInterrupt, EOFError):
+            print("")
+    else:
+        print(hex(int(str(number), 0)))
+
+
+def tobin(number=None):
+    if not number:
+        try:
+            while True:
+                print("> ", end="")
+                number = input()
+                try:
+                    print(bin(int(str(number), 0)))
+                except ValueError as e:
+                    print(e)
+        except (KeyboardInterrupt, EOFError):
+            print("")
+    else:
+        print(bin(int(str(number), 0)))
+
+
+def tooct(number=None):
+    if not number:
+        try:
+            while True:
+                print("> ", end="")
+                number = input()
+                try:
+                    print(oct(int(str(number), 0)))
+                except ValueError as e:
+                    print(e)
+        except (KeyboardInterrupt, EOFError):
+            print("")
+    else:
+        print(oct(int(str(number), 0)))
