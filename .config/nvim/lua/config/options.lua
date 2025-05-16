6922a3311a9acb07b5aca9beb725c889fd2af6a6
Tubbles <jae91m@gmail.com>
2025-05-16 15:22:51 +0200

    nvim: Add basedpyright Python LSP

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,3 +8,10 @@ opt.shiftwidth = 4
 
 -- Do not format on save by Default
 vim.g.autoformat = false
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "basedpyright"
+
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
