9d77b192411ff9de9f596828753349b233a8c668
Tubbles <jae91m@gmail.com>
2026-02-16 16:29:28 +0100

    nvim: Add ctrl+backspace,delete to remove words

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Delete word backwards with Ctrl+Backspace
+vim.keymap.set("i", "<C-h>", "<C-w>", { desc = "Delete word backwards" })
+
+-- Delete word forwards with Ctrl+Delete
+vim.keymap.set("i", "<C-Del>", "<C-o>dw", { desc = "Delete word forwards" })
