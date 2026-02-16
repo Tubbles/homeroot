1619cc38c0a2c98be6f3a23aac0ea75650e03b08
Tubbles <jae91m@gmail.com>
2026-02-16 17:20:38 +0100

    nvim: Do not show completion suggestions automatically

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,14 @@
+return {
+    "saghen/blink.cmp",
+    opts = {
+        completion = {
+            trigger = {
+                show_on_insert_on_trigger_character = false,
+                show_on_x_blocked_trigger_characters = {},
+            },
+            menu = {
+                auto_show = false,
+            },
+        },
+    },
+}
