d718b54c52f6b9faf072d4e0ef5939c66f39646a
Tubbles <jae91m@gmail.com>
2026-04-17 21:14:29 +0200

    micro: better keybinds, lsp

--- .config/micro/init.lua
+++ .config/micro/init.lua
@@ -1,3 +1,7 @@
+local buffer = import("micro/buffer")
+local micro = import("micro")
+local util = import("micro/util")
+
 function scrollUpOne(bp)
     bp:ScrollUp(1)
     return true
@@ -5,5 +9,42 @@ end
 
 function scrollDownOne(bp)
     bp:ScrollDown(1)
+    return true
+end
+
+-- VS Code-style Ctrl+D: select word under cursor, then add cursors
+-- at subsequent matches. Always does substring matching (no word
+-- boundaries) when a selection exists.
+function addNextMatch(bp)
+    local c = bp.Buf:GetCursor(bp.Buf:NumCursors() - 1)
+
+    if not c:HasSelection() then
+        c:SelectWord()
+        bp:Relocate()
+        return true
+    end
+
+    local sel = util.String(c:GetSelection())
+    local searchFrom = -c.CurSelection[2]
+
+    local match, found = bp.Buf:FindNext(
+        sel, bp.Buf:Start(), bp.Buf:End(),
+        searchFrom, true, false
+    )
+    if not found then
+        micro.InfoBar():Message("No more matches")
+        return false
+    end
+
+    local newC = bp:SpawnCursorAtLoc(match[1])
+    newC:SetSelectionStart(match[1])
+    newC:SetSelectionEnd(match[2])
+    newC.OrigSelection[1] = newC.CurSelection[1]
+    newC.OrigSelection[2] = newC.CurSelection[2]
+    newC.Loc = match[2]
+
+    bp.Buf:SetCurCursor(bp.Buf:NumCursors() - 1)
+
+    bp:Relocate()
     return true
 end
