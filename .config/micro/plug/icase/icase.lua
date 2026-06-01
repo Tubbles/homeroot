04c2b51cc0f61befd767dc56ad38d658803248b4
Tubbles <jae91m@gmail.com>
2026-06-01 17:06:46 +0200

    micro: only toggle softwrap, ignorecase locally

--- .config/micro/plug/icase/icase.lua
+++ .config/micro/plug/icase/icase.lua
@@ -1,14 +1,13 @@
 VERSION = "1.0.0"
 
 local micro = import("micro")
-local config = import("micro/config")
 
 function init()
     micro.SetStatusInfoFn("icase.indicator")
 end
 
 function indicator(b)
-    if config.GetGlobalOption("ignorecase") then
+    if b.Settings["ignorecase"] then
         return "Aa"
     end
     return "A"
